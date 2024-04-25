dc7ab69498a5141e2e7a8c51cadb98a4bd35dfbd
steGiro05 <stefanogiroletti9@gmail.com>
2024-04-25 09:12:06 +0200

    prova branch

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,10 @@ app=Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print('ciao')
     return jsonify(message='setup completed!!')
 
 if __name__=='__main__':
     app.run(debug=True)
+    
+#prova switch to back-end
