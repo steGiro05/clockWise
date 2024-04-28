c5d63164b5ba2a25e7dee936258b3bf63ee0e89f
NabilTouri <tourinabil3@gmail.com>
2024-04-28 18:50:59 +0200

    route

--- frontend/App.js
+++ frontend/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home';
-import ClassHome from './components/ClassHome';
+// In App.js in a new project
 
-const App = () => {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Home />
-      <ClassHome name = "Nabil Touri" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
